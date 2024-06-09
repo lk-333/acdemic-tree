2706d9a04cb7f77a15c888ba7924a0dacaedfe4e
李明 <92790880+StuLiMing@users.noreply.github.com>
2024-06-09 16:09:01 +0800

    .

--- api/args.py
+++ api/args.py
@@ -8,5 +8,12 @@ config.read('../config.conf')
 
 
 args={}
+args["db_host"]=config.get("Database","db_host")
+args["db_port"]=config.get("Database","db_port")
 args["db_user"]=config.get('Database', 'db_user')
 args["db_password"]=config.get('Database', 'db_password')
+args["db_database"]=config.get('Database', 'db_database')
+
+            
+
+
