6c0519f6deb0d9e068c002f2f7e81c77df02ed24
hzy138 <1525429663@qq.com>
2024-06-27 17:05:08 +0800

    x

--- api/DataStruct.py
+++ api/DataStruct.py
@@ -2,109 +2,110 @@ from database import Database
 from typing import List
 import hashlib
 import time
-from typing import Dict,Tuple
+from typing import Dict, Tuple
 import numpy as np
 import json
 
-max_id=1000
+max_id = 1000
+
 
 class Node:
-    def __init__(self,user_id=None):
-        self.user_id:int=user_id
+    def __init__(self, user_id=None):
+        self.user_id: int = user_id
         # Node+mentorship_id
-        self.mentees:List[List[Node,int]]=[]
-        self.mentors:List[List[Node,int]]=[]
-            
+        self.mentees: List[List[Node, int]] = []
+        self.mentors: List[List[Node, int]] = []
+
+
 class AcdemicTree():
-    def __init__(self,db:Database):
-        self.db=db
-        
-        db_users=db.exec("""
+    def __init__(self, db: Database):
+        self.db = db
+
+        db_users = db.exec("""
                 select * from user
                 """)
-        db_mentorships=db.exec(
+        db_mentorships = db.exec(
             """
             select * from mentorship
             """
         )
-        
-        self.users:Dict[int,Node]={}
+
+        self.users: Dict[int, Node] = {}
         for user in db_users:
-            user_node=Node(user[0])
-            self.users[user[0]]=user_node
-        
+            user_node = Node(user[0])
+            self.users[user[0]] = user_node
+
         for r in db_mentorships:
-            mentorship_id=r[0]
-            mentor_id=r[1]
-            mentee_id=r[2]
-            self.users[mentee_id].mentors.append([self.users[mentor_id],mentorship_id])
-            self.users[mentor_id].mentees.append([self.users[mentee_id],mentorship_id])
-    
+            mentorship_id = r[0]
+            mentor_id = r[1]
+            mentee_id = r[2]
+            self.users[mentee_id].mentors.append([self.users[mentor_id], mentorship_id])
+            self.users[mentor_id].mentees.append([self.users[mentee_id], mentorship_id])
+
     # 0 用户名已存在
-    # 1 注册成功             
-    def register(self,user_name,password,identity):
-        user_id=int(hashlib.sha256((user_name+password+identity).encode()).hexdigest(),16)%int(max_id)
+    # 1 注册成功
+    def register(self, user_name, password, identity):
+        user_id = int(hashlib.sha256((user_name + password + identity).encode()).hexdigest(), 16) % int(max_id)
         if self.users.get(user_id):
             return 0
-        newguy=Node(user_id=user_id)
-        self.users[user_id]=newguy
-        self.db.register(user_id=user_id,user_name=user_name,password=password,identity=identity)
+        newguy = Node(user_id=user_id)
+        self.users[user_id] = newguy
+        self.db.register(user_id=user_id, user_name=user_name, password=password, identity=identity)
         return 1
-    
-        
-    def add_mentorship(self,mentor_id,mentee_id,start_date=None,end_date=None):
-        if start_date==None:
-            start_date=time.strftime("%Y-%m-%d",time.gmtime())
-        if end_date==None:
-            end_date="1970-01-01"
-        mentorship_id=(mentee_id+mentee_id)%int(max_id)
-        self.users[mentee_id].mentors.append([self.users[mentor_id],mentorship_id])
-        self.users[mentor_id].mentees.append([self.users[mentee_id],mentorship_id])
-        self.db.add_mentorship(mentorship_id,mentor_id,mentee_id,start_date,end_date)
-    
-    def del_mentorship(self,mentor_id,mentee_id):
-        mentors_origin=self.users[mentee_id].mentors
-        mentors_new=[]
+
+    def add_mentorship(self, mentor_id, mentee_id, start_date=None, end_date=None):
+        if start_date == None:
+            start_date = time.strftime("%Y-%m-%d", time.gmtime())
+        if end_date == None:
+            end_date = "1970-01-01"
+        mentorship_id = (mentee_id + mentee_id) % int(max_id)
+        self.users[mentee_id].mentors.append([self.users[mentor_id], mentorship_id])
+        self.users[mentor_id].mentees.append([self.users[mentee_id], mentorship_id])
+        self.db.add_mentorship(mentorship_id, mentor_id, mentee_id, start_date, end_date)
+
+    def del_mentorship(self, mentor_id, mentee_id):
+        mentors_origin = self.users[mentee_id].mentors
+        mentors_new = []
         for mentor in mentors_origin:
-            if mentor[0].user_id!=mentor_id:
+            if mentor[0].user_id != mentor_id:
                 mentors_new.append(mentor)
-        self.users[mentee_id].mentors=mentors_new
-        
-        mentees_origin=self.users[mentor_id].mentees
-        mentees_new=[]
+        self.users[mentee_id].mentors = mentors_new
+
+        mentees_origin = self.users[mentor_id].mentees
+        mentees_new = []
         for mentee in mentees_origin:
-            if mentee[0].user_id!=mentee_id:
+            if mentee[0].user_id != mentee_id:
                 mentees_new.append(mentee)
-        self.users[mentor_id].mentees=mentees_new
-        self.db.del_mentorship(mentor_id,mentee_id)
-        
+        self.users[mentor_id].mentees = mentees_new
+        self.db.del_mentorship(mentor_id, mentee_id)
+
     def del_person(self):
         pass
-   
+
     # TODO:to be modified
-    def check_relationship(self,real_name):
+    def check_relationship(self, real_name):
         def id2msg2json(user_id):
-            db_ret=self.db.exec(f"""
+            db_ret = self.db.exec(f"""
                      select from user 
                      where real_name='{real_name}'
                      """)
-            real_name=db_ret[0][4]
-            profile_link=db_ret[0][5]
-            js={
-                "name":real_name,
+            real_name = db_ret[0][4]
+            profile_link = db_ret[0][5]
+            js = {
+                "name": real_name,
                 "attributes": {
-                "link": profile_link
+                    "link": profile_link
                 },
             }
             return js
-        
-        db_ret=self.db.exec(f"""
+
+        db_ret = self.db.exec(f"""
                      select from user 
                      where real_name='{real_name}'
                      """)
-        me_id=db_ret[0][0]
-        me_js=id2msg2json(me_id)
-        me_js["children"]=[id2msg2json(mentee[0].user_id for mentee in self.users[me_id].mentees)]
+        me_id = db_ret[0][0]
+        me_js = id2msg2json(me_id)
+        me_js["children"] = [id2msg2json(mentee[0].user_id for mentee in self.users[me_id].mentees)]
         return me_js
 
     # 处理申请
@@ -150,6 +151,7 @@ class AcdemicTree():
             return 1
         else:
             return 0
+
     # 查询未处理的申请条目
     def get_processed_applications(self, username: str) -> List[Dict[str, str]]:
         processed_apps = self.db.exec(f"""
@@ -160,7 +162,7 @@ class AcdemicTree():
         for app in processed_apps:
             result = {
                 "applicantName": app[0],
-                "applicantTime": app[2].strftime("%Y-%m-%d %H:%M:%S"),# 将 datetime 转换为字符串
+                "applicantTime": app[2].strftime("%Y-%m-%d %H:%M:%S"),  # 将 datetime 转换为字符串
                 "item_id": app[1]
             }
             results.append(result)
@@ -194,63 +196,80 @@ class AcdemicTree():
 
         return 1, f"Application with item_id {item_id} processed successfully with result {result}."
 
-    def locate_nodes(self,me_node:Node):
-        nodes=[]
-        links=[]
-        
-        def recursion(ments,x,y,flag,depth,fa_name):
-            N=len(ments)
-            if N%2==1:
-                x_lst=x+np.array(range(-int((N-1)/2),int((N+1)/2)+1))*100
+    # 管理员查询所有未处理的申请
+    def get_Allapplications(self) -> List[Dict[str, str]]:
+        unprocessed_apps = self.db.exec(f"""
+            SELECT applicant_name, respondent_name, item_id, creation_time FROM application 
+            WHERE is_processed = FALSE
+        """)
+        results = []
+        for app in unprocessed_apps:
+            result = {
+                "applicant_name": app[0],
+                "respondent_name": app[1],
+                "item_id": app[2],
+                "creation_time": app[3].strftime("%Y-%m-%d %H:%M:%S")
+            }
+            results.append(result)
+        return results
+
+    def locate_nodes(self, me_node: Node):
+        nodes = []
+        links = []
+
+        def recursion(ments, x, y, flag, depth, fa_name):
+            N = len(ments)
+            if N % 2 == 1:
+                x_lst = x + np.array(range(-int((N - 1) / 2), int((N + 1) / 2) + 1)) * 100
             else:
-                x_lst=x+np.array(range(-(int(N/2)),int(N/2)))*100+50
-                
-            if flag==1:
-                y_now=y-100
+                x_lst = x + np.array(range(-(int(N / 2)), int(N / 2))) * 100 + 50
+
+            if flag == 1:
+                y_now = y - 100
             else:
-                y_now=y+100 
-                
-            for x_now,node_now in zip(x_lst,ments):
-                node_now=node_now[0]
-                tmp_node_dic={}
-                s=self.db.get_user_info(node_now.user_id)
-                tmp_node_dic["real_name"]=s[4]
-                tmp_node_dic["user_id"]=node_now.user_id
-                tmp_node_dic["x"]=int(x_now)
-                tmp_node_dic["y"]=int(y_now)
+                y_now = y + 100
+
+            for x_now, node_now in zip(x_lst, ments):
+                node_now = node_now[0]
+                tmp_node_dic = {}
+                s = self.db.get_user_info(node_now.user_id)
+                tmp_node_dic["real_name"] = s[4]
+                tmp_node_dic["user_id"] = node_now.user_id
+                tmp_node_dic["x"] = int(x_now)
+                tmp_node_dic["y"] = int(y_now)
                 nodes.append(tmp_node_dic)
-                
-                tmp_link_dic={}
-                if flag==1:
-                    tmp_link_dic["source"]=s[4]
-                    tmp_link_dic["target"]=fa_name
+
+                tmp_link_dic = {}
+                if flag == 1:
+                    tmp_link_dic["source"] = s[4]
+                    tmp_link_dic["target"] = fa_name
                 else:
-                    tmp_link_dic["source"]=fa_name
-                    tmp_link_dic["target"]=s[4]
+                    tmp_link_dic["source"] = fa_name
+                    tmp_link_dic["target"] = s[4]
                 links.append(tmp_link_dic)
-                    
-                if depth>0:
-                    if flag==1:
-                        recursion(node_now.mentors,x_now,y_now,flag,depth-1,tmp_node_dic["real_name"])
+
+                if depth > 0:
+                    if flag == 1:
+                        recursion(node_now.mentors, x_now, y_now, flag, depth - 1, tmp_node_dic["real_name"])
                     else:
-                        recursion(node_now.mentees,x_now,y_now,flag,depth-1,tmp_node_dic["real_name"])
-                        
-        me_x=500
-        me_y=300
+                        recursion(node_now.mentees, x_now, y_now, flag, depth - 1, tmp_node_dic["real_name"])
+
+        me_x = 500
+        me_y = 300
         me_dic = {}
         s = self.db.get_user_info(me_node.user_id)
         me_dic["real_name"] = s[4]
         me_dic["user_id"] = me_node.user_id
         me_dic["profile_link"] = s[5]
-        me_dic["x"]=me_x
-        me_dic["y"]=me_y
+        me_dic["x"] = me_x
+        me_dic["y"] = me_y
         nodes.append(me_dic)
-        
-        recursion(me_node.mentors,me_x,me_y,1,1,me_dic["real_name"])
-        recursion(me_node.mentees,me_x,me_y,0,1,me_dic["real_name"])
-        
-        data={}
-        data["nodes"]=nodes
-        data["links"]=links
+
+        recursion(me_node.mentors, me_x, me_y, 1, 1, me_dic["real_name"])
+        recursion(me_node.mentees, me_x, me_y, 0, 1, me_dic["real_name"])
+
+        data = {}
+        data["nodes"] = nodes
+        data["links"] = links
         return data
 
--- api/api.py
+++ api/api.py
@@ -5,149 +5,163 @@ from flask_cors import CORS
 import queue
 import time
 from args import args
-from DataStruct import AcdemicTree,Node
+from DataStruct import AcdemicTree, Node
 from typing import List
 import numpy as np
 
-# to_pre_queue = queue.Queue() 
+
+# to_pre_queue = queue.Queue()
 # from_pre_quque = queue.Queue()  
-    
+
 def locate_nodes(me_node: Node):
-    nodes=[]
-    links=[]
-    
-    def recursion(ments:List[List[Node, int]],x,y,flag,depth,fa_name):
-        N=len(ments)
-        if N%2==1:
-            x_lst=np.array(range(-int((N-1)/2),int((N+1)/2)+1)*100)
+    nodes = []
+    links = []
+
+    def recursion(ments: List[List[Node, int]], x, y, flag, depth, fa_name):
+        N = len(ments)
+        if N % 2 == 1:
+            x_lst = np.array(range(-int((N - 1) / 2), int((N + 1) / 2) + 1) * 100)
         else:
-            x_lst=np.array(range(-(int(N/2)),int(N/2)))*100+50
-            
-        if flag==1:
-            y_now=y-100
+            x_lst = np.array(range(-(int(N / 2)), int(N / 2))) * 100 + 50
+
+        if flag == 1:
+            y_now = y - 100
         else:
-            y_now=y+100 
-            
-        for x_now,node_now in zip(x_lst,ments):
-            node_now=node_now[0]
-            tmp_node_dic={}
-            s=db.get_user_info(node_now.user_id)
-            tmp_node_dic["real_name"]=s[4]
-            tmp_node_dic["user_id"]=node_now.user_id
-            tmp_node_dic["x"]=x_now
-            tmp_node_dic["y"]=y_now
+            y_now = y + 100
+
+        for x_now, node_now in zip(x_lst, ments):
+            node_now = node_now[0]
+            tmp_node_dic = {}
+            s = db.get_user_info(node_now.user_id)
+            tmp_node_dic["real_name"] = s[4]
+            tmp_node_dic["user_id"] = node_now.user_id
+            tmp_node_dic["x"] = x_now
+            tmp_node_dic["y"] = y_now
             nodes.append(tmp_node_dic)
-            
-            tmp_link_dic={}
-            if flag==1:
-                tmp_link_dic["source"]=s[4]
-                tmp_link_dic["target"]=fa_name
+
+            tmp_link_dic = {}
+            if flag == 1:
+                tmp_link_dic["source"] = s[4]
+                tmp_link_dic["target"] = fa_name
             else:
-                tmp_link_dic["source"]=fa_name
-                tmp_link_dic["target"]=s[4]
+                tmp_link_dic["source"] = fa_name
+                tmp_link_dic["target"] = s[4]
             links.append(tmp_link_dic)
-                
-            if depth>0:
-                if flag==1:
-                    recursion(node_now.mentors,x_now,y_now,flag,depth-1)
+
+            if depth > 0:
+                if flag == 1:
+                    recursion(node_now.mentors, x_now, y_now, flag, depth - 1)
                 else:
-                    recursion(node_now.mentees,x_now,y_now,flag,depth-1)
-                       
-    me_x=500
-    me_y=300
+                    recursion(node_now.mentees, x_now, y_now, flag, depth - 1)
+
+    me_x = 500
+    me_y = 300
     me_dic = {}
     s = db.get_user_info(me_node.user_id)
     me_dic["real_name"] = s[4]
     me_dic["user_id"] = me_node.user_id
     me_dic["profile_link"] = s[5]
-    me_dic["x"]=me_x
-    me_dic["y"]=me_y
+    me_dic["x"] = me_x
+    me_dic["y"] = me_y
     nodes.append(me_dic)
-    
-    recursion(me_node.mentors,me_x,me_y,1,1,me_dic["real_name"])
-    recursion(me_node.mentees,me_x,me_y,0,1,me_dic["real_name"])
-    
-    data={}
-    data["ndoes"]=nodes
-    data["links"]=links
+
+    recursion(me_node.mentors, me_x, me_y, 1, 1, me_dic["real_name"])
+    recursion(me_node.mentees, me_x, me_y, 0, 1, me_dic["real_name"])
+
+    data = {}
+    data["ndoes"] = nodes
+    data["links"] = links
     return data
-
-
 
 
 def back():
     while True:
         time.sleep(1000)
 
+
 def run_back_thread():
-    th=threading.Thread(target=back,)
+    th = threading.Thread(target=back, )
     th.start()
     return th
 
+
 app = Flask(__name__)
-db=Database()
-atree=AcdemicTree(db=db)
+db = Database()
+atree = AcdemicTree(db=db)
 
 
-@app.route('/check_user',methods=['POST'])
+@app.route('/check_user', methods=['POST'])
 def check_user():
-    data=request.get_json()
-    status=db.check_user(user_name=data["username"],password=data['password'],identity=data['identity'])
-    return jsonify({'status':status})
+    data = request.get_json()
+    status = db.check_user(user_name=data["username"], password=data['password'], identity=data['identity'])
+    return jsonify({'status': status})
 
-@app.route("/register_user",methods=['POST']) 
+
+@app.route("/register_user", methods=['POST'])
 def register_user():
-    data=request.get_json()
-    status=atree.register(user_name=data["username"],password=data['password'],identity=data['identity'])
-    return jsonify({'status':status})
+    data = request.get_json()
+    status = atree.register(user_name=data["username"], password=data['password'], identity=data['identity'])
+    return jsonify({'status': status})
 
-@app.route("/send-application",methods=['POST'])
+
+@app.route("/send-application", methods=['POST'])
 def send_application():
-    data=request.get_json()
-    status,s=atree.apply_for_mentorship(applicant_name=data["yourName"],respondent_name=data['otherName'])
-    return jsonify({'status':status})
+    data = request.get_json()
+    status, s = atree.apply_for_mentorship(applicant_name=data["yourName"], respondent_name=data['otherName'])
+    return jsonify({'status': status})
 
-@app.route("/update-profile",methods=['POST'])
+
+@app.route("/update-profile", methods=['POST'])
 def update_profile():
-    data=request.get_json()
-    status,s=atree.update_personal_info(username = data['username'], realName = data['realName'], homepage = data['homepage'])
-    return jsonify({'status':status})
+    data = request.get_json()
+    status, s = atree.update_personal_info(username=data['username'], realName=data['realName'],
+                                           homepage=data['homepage'])
+    return jsonify({'status': status})
 
 
-@app.route("/search_user",methods=['POST'])
+@app.route("/search_user", methods=['POST'])
 def search_user():
-    data=request.get_json()
-    status=atree.check_username_exists(username = data['name'])
-    return jsonify({'status':status})
+    data = request.get_json()
+    status = atree.check_username_exists(username=data['name'])
+    return jsonify({'status': status})
 
-@app.route("/get-applications",methods=['POST'])
+
+@app.route("/get-applications", methods=['POST'])
 def get_applications():
-    data=request.get_json()
-    s=atree.get_processed_applications(username = data['username'])
-    return jsonify({'applications':s})
+    data = request.get_json()
+    s = atree.get_processed_applications(username=data['username'])
+    return jsonify({'applications': s})
 
-@app.route("/deal-applications",methods=['POST'])
+
+@app.route("/deal-applications", methods=['POST'])
 def deal_applications():
-    data=request.get_json()
-    status,s=atree.process_application(item_id = data['item_id'], result = data['result'])
+    data = request.get_json()
+    status, s = atree.process_application(item_id=data['item_id'], result=data['result'])
     print(data['item_id'])
     print(data['result'])
-    return jsonify({'status':status})
+    return jsonify({'status': status})
 
-@app.route('/bulid_tree',methods=['POST'])
+
+@app.route('/bulid_tree', methods=['POST'])
 def bulid_tree():
     data = request.get_json()
-    user_node=atree.users[int(data['id'])]
-    tree=atree.locate_nodes(user_node)
+    user_node = atree.users[int(data['id'])]
+    tree = atree.locate_nodes(user_node)
     return jsonify(tree)
 
 
+@app.route("/get-Allapplications", methods=['POST'])
+def get_Allapplications():
+    data = request.get_json()
+    s = atree.get_Allapplications()
+    return jsonify({'applications': s})
 
-if __name__=="__main__":   
+
+if __name__ == "__main__":
     def run():
         app.run(debug=True)
-    
+
+
     run()
-    back_thread=run_back_thread()
-    
-    
+    back_thread = run_back_thread()
+
